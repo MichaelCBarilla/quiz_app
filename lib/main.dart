72d46eb4c2911e214192cdab9c8751b6b89fded8
Michael Barilla <michael@eeproto.com>
2022-11-29 21:14:05 -0500

    split up into smaller widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,65 +15,61 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        'Black',
+        'Red',
+        'Green',
+        'White',
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        'Rabbit',
+        'Snake',
+        'Turtle',
+        'Lion',
+      ]
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        'Max',
+        'Maxxx',
+        'MAX',
+        'max...',
+      ]
+    },
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex += 1;
     });
-    print('Answer Chosen!');
+    if (_questionIndex < _questions.length) {
+      print('We have more questions');
+    } else {
+      print('no more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': [
-          'Black',
-          'Red',
-          'Green',
-          'White',
-        ]
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': [
-          'Rabbit',
-          'Snake',
-          'Turtle',
-          'Lion',
-        ]
-      },
-      {
-        'questionText': 'Who\'s your favorite instructor?',
-        'answers': [
-          'Max',
-          'Maxxx',
-          'MAX',
-          'max...',
-        ]
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Quiz App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questionText: questions[_questionIndex]['questionText'] as String,
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(
-                answerText: answer,
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
                 selectHandler: _answerQuestion,
-              );
-            }).toList()
-          ],
-        ),
+                questionIndex: _questionIndex)
+            : const Result(),
       ),
     );
   }
